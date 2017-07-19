273b4b4e4168493a4deef1ebc46a8c9bfa2ff1b8
gapon <gapon@T1000.local>
2017-07-19 13:59:19 +0300

    add Makefile

--- src/data/download.py
+++ src/data/download.py
@@ -1,6 +1,13 @@
 import requests
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-filename = 'data/raw/iris.csv'
-with open(filename, 'wb') as ofile:
+import click
+
+@click.command()
+@click.argument('url')
+@click.argument('filename', type=click.Path())
+def download_file(url, filename):
+   print('Downloading from {} to {}'.format(url, filename))
    response = requests.get(url)
-   file.write(response.content)
+   with open(filename,  'wb') as ofile:
+       ofile.write(response.content)
+if __name__ == '__main__':
+   download_file()
